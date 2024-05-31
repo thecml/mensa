766737a44294eb59b59859343b954d6ef2eafefb
thecml <chr1000@gmail.com>
2024-05-31 14:11:12 -0600

    Used clayton

--- src/models/train_mensa_model.py
+++ src/models/train_mensa_model.py
@@ -8,13 +8,13 @@ import random
 import warnings
 from models import Mensa
 from multi_evaluator import MultiEventEvaluator
-from data_loader import RotterdamDataLoader, ALSDataLoader
+from data_loader import *
 from utility.survival import preprocess_data
 from utility.data import dotdict
 import torch.optim as optim
 import torch.nn as nn
-from copula.Frank import Frank
 from data_loader import get_data_loader
+from src.copula import Clayton
 
 warnings.filterwarnings("ignore", message=".*The 'nopython' keyword.*")
 
@@ -28,10 +28,10 @@ device = torch.device(device)
 
 if __name__ == "__main__":
     # Load data
-    dl = RotterdamDataLoader().load_data()
+    dl = SyntheticDataLoader().load_data()
     num_features, cat_features = dl.get_features()
     data_packages = dl.split_data(train_size=0.7, valid_size=0.5)
-    n_events = 2
+    n_events = 3
     n_output = 1
     n_hidden = 100
     
@@ -67,6 +67,17 @@ if __name__ == "__main__":
     data_test["y1_event"] = pd.Series(test_data[2][:,0])
     data_test["y2_event"] = pd.Series(test_data[2][:,1])
     
+    # Create censoring event
+    data_train['y3_time'] = data_train.apply(lambda x: x['y1_time'] if (x['y1_event'] == 0)
+                                             & (x['y2_event'] == 0) else 0, axis=1)
+    data_train['y3_event'] = data_train.apply(lambda x: 1 if x['y3_time'] != 0 else 0, axis=1)
+    data_valid['y3_time'] = data_valid.apply(lambda x: x['y1_time'] if (x['y1_event'] == 0)
+                                             & (x['y2_event'] == 0) else 0, axis=1)
+    data_valid['y3_event'] = data_valid.apply(lambda x: 1 if x['y3_time'] != 0 else 0, axis=1)
+    data_test['y3_time'] = data_test.apply(lambda x: x['y1_time'] if (x['y1_event'] == 0)
+                                             & (x['y2_event'] == 0) else 0, axis=1)
+    data_test['y3_event'] = data_test.apply(lambda x: 1 if x['y3_time'] != 0 else 0, axis=1)                                         
+    
     # Train neural model to learn non-linear network weights.
     model = train_mensa_model(model, data_train, data_valid, time_bins, config=config,
                               random_state=0, reset_model=True, device=device)
@@ -75,6 +86,8 @@ if __name__ == "__main__":
     evaluator = MultiEventEvaluator(data_test, data_train, model, config, device)
     surv_preds = evaluator.predict_survival_curves()
     for event_id in range(n_events):
+        if event_id == 2:
+            break
         ci = evaluator.calculate_ci(surv_preds[event_id], event_id)
         mae = evaluator.calculate_mae(surv_preds[event_id], event_id, method="Hinge")
         print(f"Event {event_id} - CI={round(ci, 2)} - MAE={round(mae, 2)}")
@@ -86,7 +99,7 @@ if __name__ == "__main__":
     model.fc2 = nn.Linear(n_hidden, n_output)
     
     # Retrain model with copula
-    copula = Frank(torch.tensor([0.1], device=device).type(torch.float32), device=device)
+    copula = Clayton(torch.tensor([0.1], device=device).type(torch.float32), device=device)
     model = train_mensa_model(model, data_train, data_valid, time_bins, config=config,
                               random_state=0, reset_model=True, device=device, copula=copula)
     
