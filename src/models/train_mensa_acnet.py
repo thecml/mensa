cda76f0b055c76f2a98b37494b79d9da5f253ddc
thecml <chr1000@gmail.com>
2024-06-06 14:08:03 -0600

    Added synthetic dataloaders

--- src/models/train_mensa_acnet.py
+++ src/models/train_mensa_acnet.py
@@ -10,12 +10,10 @@ from models import Mensa
 from multi_evaluator import MultiEventEvaluator
 from data_loader import *
 from utility.survival import preprocess_data
-from utility.data import dotdict
+from utility.data import dotdict, array_to_tensor
 import torch.optim as optim
 import torch.nn as nn
-from data_loader import get_data_loader
 from copula import Clayton
-from data.synth_data import linear_dgp_parametric_ph
 from utility.survival import convert_to_structured
 from dcsurvival.dirac_phi import DiracPhi
 from dcsurvival.survival import DCSurvival
@@ -30,23 +28,19 @@ np.random.seed(0)
 torch.manual_seed(0)
 random.seed(0)
 
+# Setup precision
+torch.set_default_dtype(torch.float64)
+
 # Setup device
 device = "cpu" # use CPU
 device = torch.device(device)
-
-def array_to_tensor(array, dtype=None, device='cpu'):
-    if not isinstance(array, np.ndarray):
-        array = np.array(array)
-    array_c = array.copy()
-    tensor = torch.tensor(array_c, dtype=dtype).to(device)
-    return tensor
 
 def predict_survival_curve(model, x_test, time_bins, truth=False):
     device = torch.device("cpu")
     if truth == False:
         model = copy.deepcopy(model).to(device)
     surv_estimate = torch.zeros((x_test.shape[0], time_bins.shape[0]), device=device)
-    x_test = torch.tensor(x_test)
+    x_test = torch.tensor(x_test, dtype=torch.float32)
     time_bins = torch.tensor(time_bins)
     for i in range(time_bins.shape[0]):
         surv_estimate[:,i] = model.survival(time_bins[i], x_test)
@@ -54,26 +48,19 @@ def predict_survival_curve(model, x_test, time_bins, truth=False):
 
 if __name__ == "__main__":
     # Load data
-    df, params = linear_dgp_parametric_ph(copula_name="Frank",
-                                          n_features=10,
-                                          n_samples=1000)
-    X = df.drop(['observed_time', 'event_indicator',
-                 'event_time', 'censoring_time'], axis=1)
-    y = convert_to_structured(df['observed_time'].values,
-                              df['event_indicator'].values)
-    cat_features = []
-    num_features = list(X.columns)
+    dl = LinearSyntheticDataLoader().load_data(n_samples=1000)
+    num_features, cat_features = dl.get_features()
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = dl.split_data(train_size=0.7,
+                                                                             valid_size=0.5)
+    beta_e, beta_c = dl.params
     
-    # Split data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.25,
-                                                          random_state=0)
     # Make time bins
     time_bins = make_time_bins(y_train['time'], event=y_train['event'])
 
     # Scale data
     X_train, X_valid, X_test = preprocess_data(X_train, X_valid, X_test,
-                                               cat_features, num_features, as_array=True)
+                                               cat_features, num_features,
+                                               as_array=True)
     
     # Format data
     times_tensor_train = array_to_tensor(y_train['time'], torch.float32)
@@ -84,15 +71,15 @@ if __name__ == "__main__":
     covariate_tensor_val = torch.tensor(X_valid).to(device)
     
     # Define ACNet, model
-    depth = 2 # depth of phi_nn
-    widths = [100, 100] # number of units of phi_nn
+    depth = 2 # depth of ACNet
+    widths = [100, 100] # number of units of ACNet
     lc_w_range = (0, 1.0) # Phi_B
     shift_w_range = (0., 2.0) # Phi_B
     num_epochs = 100 # 5000
     batch_size = 128
     early_stop_epochs = 10
     phi = DiracPhi(depth, widths, lc_w_range, shift_w_range, device, tol=1e-14).to(device)
-    model = DCSurvival(phi, device = device, num_features=X.shape[1], tol=1e-14).to(device)
+    model = DCSurvival(phi, device = device, num_features=X_train.shape[1], tol=1e-14).to(device)
     optimizer = optim.Adam([{"params": model.sumo_e.parameters(), "lr": 1e-3},
                             {"params": model.sumo_c.parameters(), "lr": 1e-3},
                             {"params": model.phi.parameters(), "lr": 1e-4}])
@@ -131,7 +118,6 @@ if __name__ == "__main__":
     print(f"DCSurvial: CI={round(ci, 2)} - MAE={round(mae_hinge, 2)} - IBS={round(ibs, 2)}")
     
     # Train and evaluate truth model
-    beta_e = params[0]
     truth_model = Weibull_linear(num_feature=X_test.shape[1], shape=4,
                                  scale=14, device=torch.device("cpu"),
                                  coeff=beta_e)
