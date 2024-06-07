697a2907abe0360033a2b62f4139c056c7d1bb1f
aligharari96 <hosseing@ualberta.ca>
2024-06-07 10:39:52 -0600

    optimization test

--- test_script.py
+++ test_script.py
@@ -1,5 +1,7 @@
 import numpy as np 
 import torch
+import matplotlib.pyplot as plt
+from pycop import simulation
 
 
 
@@ -8,14 +10,57 @@ import torch
 def LOG(x):
     return torch.log(x+1e-20*(x<1e-20))
 
-def generate_events(dgp1, dgp2, dgp3, x, device,copula=None):
-    if copula is None:
-        uv = torch.rand((x.shape[0],3), device=device)
+class Clayton:
+    def __init__(self, theta, epsilon=1e-3, device='cpu'):
+        self.theta = theta
+        self.eps = torch.tensor([epsilon], device=device).type(torch.float32)
+        self.device = device
+
+    def CDF(self, u):#(sum(ui**-theta)-2)**(-1/theta)
+        u = u + 1e-30*(u<1e-30)
+        u = u.clamp(0,1)
+        #tmp = torch.sum(u**(-1.0*self.theta), dim=1)-1
+        tmp = torch.exp(-self.theta * LOG(u))
+        tmp = torch.sum(tmp, dim=1)-2
+        return torch.exp((-1/self.theta)*LOG(tmp))
+    
+    def conditional_cdf(self, condition_on, uv):
+        uv_eps = torch.empty_like(uv, device=self.device)
+        if condition_on == "u":
+            uv_eps[:,0] = uv[:,0] + self.eps
+            uv_eps[:,1] = uv[:,1]
+            uv_eps[:,2] = uv[:,2]
+        elif condition_on == 'v':
+            uv_eps[:,1] = uv[:,1] + self.eps
+            uv_eps[:,0] = uv[:,0]
+            uv_eps[:,2] = uv[:,2]
+        else:
+            uv_eps[:,2] = uv[:,2] + self.eps
+            uv_eps[:,1] = uv[:,1]
+            uv_eps[:,0] = uv[:,0]
+
+
+        return (self.CDF(uv_eps) - self.CDF(uv))/self.eps
+    
+    def enable_grad(self):
+        self.theta.requires_grad = True
+
+    
+
+def generate_events(dgp1, dgp2, dgp3, x, device,theta=2.0, family='clayton'):
+    if family is None:
+        uv = torch.randn((x.shape[0], 3))#sample idnependent 
     else:
-        uv = copula.rvs(x.shape[0])
+        u,v,w = simulation.simu_archimedean(family, 3, x.shape[0], theta=theta)
+        u = torch.from_numpy(u).type(torch.float32).reshape(-1,1)
+        v = torch.from_numpy(v).type(torch.float32).reshape(-1,1)
+        w = torch.from_numpy(w).type(torch.float32).reshape(-1,1)
+        
+        uv = torch.cat([u,v,w], axis=1)
+
     t1 = dgp1.rvs(x, uv[:,0])
     t2 = dgp2.rvs(x, uv[:,1])
-    t3 = dgp3.rvs(x, uv[:,0])
+    t3 = dgp3.rvs(x, uv[:,2])
     T = np.concatenate([t1.reshape(-1,1),t2.reshape(-1,1),t3.reshape(-1,1)], axis=1)
     E = np.argmin(T,axis=1)
     obs_T = T[np.arange(T.shape[0]), E]
@@ -31,14 +76,14 @@ def synthetic_x(n_train, n_val, n_test, nf, device):
     x_test = torch.rand((n_test, nf), device=device)
     return {"x_train":x_train, "x_val":x_val, "x_test":x_test}
 
-def generate_data(x_dict, dgp1, dgp2,dgp3,device, copula=None):
-    train_dict = generate_events(dgp1, dgp2,dgp3, x_dict['x_train'],device, copula)
-    val_dict = generate_events(dgp1, dgp2,dgp3, x_dict['x_val'],device, copula)
-    test_dict = generate_events(dgp1, dgp2,dgp3, x_dict['x_test'],device, copula)
+def generate_data(x_dict, dgp1, dgp2,dgp3,device, copula='clayton', theta=2.0):
+    train_dict = generate_events(dgp1, dgp2,dgp3, x_dict['x_train'],device, theta, copula)
+    val_dict = generate_events(dgp1, dgp2,dgp3, x_dict['x_val'],device, theta, copula)
+    test_dict = generate_events(dgp1, dgp2,dgp3, x_dict['x_test'],device, theta, copula)
     return train_dict, val_dict, test_dict
 
 
-def loss_triple(model1, model2, model3, data, copula=None):
+def loss_triple(model1, model2, model3, data, copula=None):#estimates the joint loss
     s1 = model1.survival(data['T'], data['X'])
     s2 = model2.survival(data['T'], data['X'])
     s3 = model3.survival(data['T'], data['X'])
@@ -52,9 +97,10 @@ def loss_triple(model1, model2, model3, data, copula=None):
         p3 = LOG(f3) + LOG(s1) + LOG(s2)
     else:
         
-        S = torch.cat([s1.reshape(-1,1), s2.reshape(-1,1)], dim=1).clamp(0.001,0.999)
+        S = torch.cat([s1.reshape(-1,1), s2.reshape(-1,1), s3.reshape(-1,1)], dim=1).clamp(0.001,0.999)
         p1 = LOG(f1) + LOG(copula.conditional_cdf("u", S))
         p2 = LOG(f2) + LOG(copula.conditional_cdf("v", S))
+        p3 = LOG(f3) + LOG(copula.conditional_cdf("w", S))
     p1[torch.isnan(p1)] = 0
     p2[torch.isnan(p2)] = 0
     p3[torch.isnan(p3)] = 0
@@ -65,17 +111,11 @@ def loss_triple(model1, model2, model3, data, copula=None):
     loss = -loss/data['E'].shape[0]
     return loss
 
-def single_loss(model, data, event_name='t1'):
+def single_loss(model, data, event_name='t1'):#estimates loss assuming every thing is observed/no censoring for checking 
     f = model.PDF(data[event_name], data['X'])
     return -torch.mean(LOG(f))
 
-def single_loss_2(model, data, e):
-    s = model.survival(data['T'], data['X'])
-    f = model.PDF(data['T'], data['X'])
-    
-    E = (data['E'] == e).type(torch.float32)
-    
-    return -torch.mean((E * LOG(f)) + ((1-E)*LOG(s)))
+
 
 class Exp_linear:
     def __init__(self, bh, nf) -> None:
@@ -134,52 +174,103 @@ if __name__ == "__main__":
     DEVICE = 'cpu'
     torch.manual_seed(0)
     np.random.seed(0)
-    nf = 5
+    nf = 1
     n_train = 10000
     n_val = 5000
     n_test = 5000
     x_dict = synthetic_x(n_train, n_val, n_test, nf, DEVICE)
     dgp1 = Exp_linear(0.1, nf)
-    dgp2 = Exp_linear(0.04, nf)
+    dgp2 = Exp_linear(0.06, nf)
     dgp3 = Exp_linear(0.07, nf)
     dgp1.coeff = torch.rand((nf,),device=DEVICE)
     dgp2.coeff = torch.rand((nf,), device=DEVICE)
     dgp3.coeff = torch.rand((nf,), device=DEVICE)
 
 
-    copula_dgp = None
+    copula_dgp = 'clayton'
+    theta_dgp = 2.0
+    eps = 1e-4
 
     train_dict, val_dict, test_dict = \
-                generate_data(x_dict, dgp1, dgp2, dgp3,DEVICE, copula_dgp)
+                generate_data(x_dict, dgp1, dgp2, dgp3,DEVICE, copula_dgp, theta_dgp)
+    
+    #remove the comment to check the percentage of each event 
+    #plt.hist(train_dict['E'])
+    #plt.show()
+    #assert 0
+
+    #copula for estimation
+    copula_start_point = 8.0
+    copula = Clayton(torch.tensor([copula_start_point]),eps, DEVICE)
     
     indep_model1 = Exp_linear_model(0.1, nf)
     indep_model2 = Exp_linear_model(0.1, nf)
     indep_model3 = Exp_linear_model(0.1, nf)
-
+    
     indep_model1.enable_grad()
     indep_model2.enable_grad()
     indep_model3.enable_grad()
+    copula.enable_grad()
+    optimizer = torch.optim.Adam(indep_model3.parameters(), lr=5e-3)
 
-    model_optimizer = torch.optim.Adam( list(indep_model1.parameters())+list(indep_model2.parameters() + list(indep_model3.parameters())), lr=1e-3, weight_decay=0.01)
-    for i in range(10000):
-        model_optimizer.zero_grad()
-        loss = loss_triple(indep_model1, indep_model2, indep_model3, train_dict)
+    optimizer = torch.optim.Adam([  {"params": indep_model1.parameters(), "lr": 1e-2},
+                                    {"params": indep_model2.parameters(), "lr": 1e-2},
+                                    {"params": indep_model3.parameters(), "lr": 1e-2},
+                                ])
+    #add pretraining to make sure its possible to converge to the correct model
+    pre_trainn_epochs = 200
+    for i in range(pre_trainn_epochs):
+        optimizer.zero_grad()
+        loss = single_loss(indep_model1, train_dict, 't1')
         loss.backward()
-        model_optimizer.step()
-        print(loss)
+        optimizer.step()
+    print("Event 1--> trained model: ", single_loss(indep_model1, val_dict, 't1'), "\tDGP:", single_loss(dgp1, val_dict, 't1'))
 
 
-    #NLL of all of the events together --> problem
-    print(loss_triple(dgp1, dgp2, dgp3, val_dict))
-    print(loss_triple(indep_model1, indep_model2, indep_model3, val_dict))
+    for i in range(pre_trainn_epochs):
+        optimizer.zero_grad()
+        loss = single_loss(indep_model2, train_dict, 't2')
+        loss.backward()
+        optimizer.step()
+    print("Event 2--> trained model: ", single_loss(indep_model2, val_dict, 't2'), "\tDGP:", single_loss(dgp2, val_dict, 't2'))
 
-    #NLL assuming every thing is observed, here NLL works correctly
-    print(single_loss(dgp1, val_dict, 't1'), single_loss(indep_model1, val_dict, 't1'))
-    print(single_loss(dgp2, val_dict, 't2'), single_loss(indep_model2, val_dict, 't2'))
-    print(single_loss(dgp3, val_dict, 't3'), single_loss(indep_model3, val_dict, 't3'))
 
-    #NLL for each event assuming two other events as censoring--> problematic here as well 
-    print(single_loss_2(dgp1, val_dict, 0), single_loss_2(indep_model1, val_dict, 0))
-    print(single_loss_2(dgp2, val_dict, 1), single_loss_2(indep_model2, val_dict, 1))
-    print(single_loss_2(dgp3, val_dict, 2), single_loss_2(indep_model3, val_dict, 2))
+    for i in range(pre_trainn_epochs):
+        optimizer.zero_grad()
+        loss = single_loss(indep_model3, train_dict, 't3')
+        loss.backward()
+        optimizer.step()
+    print("Event 3--> trained model: ", single_loss(indep_model3, val_dict, 't3'), "\tDGP:", single_loss(dgp3, val_dict, 't3'))
+
+    #training loop
+
+    optimizer = torch.optim.Adam([  {"params": indep_model1.parameters(), "lr": 1e-2},
+                                    {"params": indep_model2.parameters(), "lr": 1e-2},
+                                    {"params": indep_model3.parameters(), "lr": 1e-2},
+                                    {"params": [copula.theta], "lr": 1e-3}
+                                ])
+    n_epochs = 10000
+    for i in range(n_epochs):
+        optimizer.zero_grad()
+        loss = loss_triple(indep_model1, indep_model2, indep_model3, train_dict, copula)
+        loss.backward()
+        
+        copula.theta.grad = copula.theta.grad*1000
+        #play with the clip range to see if it makes any differences 
+        copula.theta.grad.clamp_(torch.tensor([-0.5]), torch.tensor([0.5]))
+        optimizer.step()
+        if i%500 == 0:
+            print(loss, copula.theta)
+
+    print("###############################################################")
+    #NLL of all of the events together
+    print(loss_triple(indep_model1, indep_model2, indep_model3, val_dict, copula))
+    #check the dgp performance
+    copula.theta = torch.tensor([theta_dgp])
+    print(loss_triple(dgp1, dgp2, dgp3, val_dict, copula))
     
+    
+    #NLL assuming every thing is observed, here NLL works correctly
+    print("Event 1--> trained model: ", single_loss(indep_model1, val_dict, 't1'), "\tDGP:", single_loss(dgp1, val_dict, 't1'))
+    print("Event 2--> trained model: ", single_loss(indep_model2, val_dict, 't2'), "\tDGP:", single_loss(dgp2, val_dict, 't2'))
+    print("Event 3--> trained model: ", single_loss(indep_model3, val_dict, 't3'), "\tDGP:", single_loss(dgp3, val_dict, 't3'))
