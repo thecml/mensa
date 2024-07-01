20cad7c042657261088894ecae8c64611115bf34
thecml <chr1000@gmail.com>
2024-07-01 13:06:32 -0600

    Loaded mimic iv dataset

--- src/tests/test_data_loader.py
+++ src/tests/test_data_loader.py
@@ -6,9 +6,7 @@ from utility.config import load_config
 import config as cfg
 
 if __name__ == '__main__':
-    dl = MimicDataLoader().load_data()
-    
-    data_config = load_config(cfg.DATA_CONFIGS_DIR, f"synthetic.yaml")
+    data_config = load_config(cfg.DGP_CONFIGS_DIR, f"synthetic.yaml")
     dl = SingleEventSyntheticDataLoader().load_data(data_config, k_tau=0.25)
     train_dict, valid_dict, test_dict = dl.split_data(train_size=0.7, valid_size=0.1, test_size=0.2)
     print(train_dict['X'].shape)
