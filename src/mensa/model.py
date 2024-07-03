cc842c4a4dae2da95f59b5eaae7d9ab479e00d22
thecml <chr1000@gmail.com>
2024-07-03 16:11:27 -0600

    Added loss fn and moved nde

--- src/mensa/model.py
+++ src/mensa/model.py
@@ -19,6 +19,125 @@ from copula import Clayton2D, Frank2D, Clayton
 
 from mensa.loss import (calculate_loss_two_models, calculate_loss_three_models, calculate_loss_three_models_me)
 
+def make_mensa_model_2_events(n_features, start_theta, eps, device, dtype):
+    model1 = Weibull_log_linear(n_features, 2, 1, device, dtype)
+    model2 = Weibull_log_linear(n_features, 2, 1, device, dtype)
+    copula = Clayton2D(torch.tensor([start_theta], dtype=dtype), device, dtype)
+    return model1, model2, copula
+
+def make_mensa_model_3_events(n_features, start_theta, eps, device, dtype):
+    # Create models and copula
+    #copula = Clayton.Clayton3(torch.tensor([2.0], dtype=dtype), eps, dtype, device)
+    #copula = NestedFrank(torch.tensor([copula_start_point]),
+    #                     torch.tensor([copula_start_point]), eps, eps, device, dtype)
+    #c1 = NestedFrank(torch.tensor([2.0]), torch.tensor([2.0]), 1e-4, 1e-4, device, dtype)
+    #copula = NestedClayton(torch.tensor([2.0]), torch.tensor([2.0]), 1e-4, 1e-4, device, dtype)
+    #copula = ConvexCopula(c1, c2, beta=10000, device=device, dtype=dtype)
+    model1 = Weibull_log_linear(n_features, 2, 1, device, dtype)
+    model2 = Weibull_log_linear(n_features, 2, 1, device, dtype)
+    model3 = Weibull_log_linear(n_features, 2, 1, device, dtype)
+    copula = NestedClayton(torch.tensor([start_theta]), torch.tensor([start_theta]), eps, eps, device, dtype)
+    return model1, model2, model3, copula
+
+def train_mensa_model_2_events(train_dict, valid_dict, model1, model2, copula, n_epochs=1000, lr=5e-3):
+    model1.enable_grad()
+    model2.enable_grad()
+    copula.enable_grad()
+    
+    optimizer = torch.optim.Adam([{"params": model1.parameters(), "lr": lr},
+                                  {"params": model2.parameters(), "lr": lr},
+                                  {"params": copula.parameters(), "lr": lr}])
+    
+    min_val_loss = 1000
+    for itr in range(n_epochs):
+        optimizer.zero_grad()
+        loss = calculate_loss_two_models(model1, model2, train_dict, copula)
+        loss.backward()
+        
+        for p in copula.parameters():
+            p.grad = p.grad * 100
+            p.grad.clamp_(torch.tensor([-0.5]), torch.tensor([0.5]))
+        
+        optimizer.step()
+        
+        """
+        for p in copula.parameters():
+            if p <= 0.01:
+                with torch.no_grad():
+                    p[:] = torch.clamp(p, 0.01, 100)
+        """
+        
+        with torch.no_grad():
+            val_loss = calculate_loss_two_models(model1, model2, valid_dict, copula)
+            if itr % 100 == 0:
+                print(f"{val_loss} - {copula.theta}")
+            if not torch.isnan(val_loss) and val_loss < min_val_loss:
+                stop_itr = 0
+                
+                best_c1 = model1.coeff.detach().clone()
+                best_c2 = model2.coeff.detach().clone()
+                best_mu1 = model1.mu.detach().clone()
+                best_mu2 = model2.mu.detach().clone()
+                best_sig1 = model1.sigma.detach().clone()
+                best_sig2 = model2.sigma.detach().clone()
+                
+                """
+                best_bh1 = model1.bh.detach().clone()
+                best_coeff1 = model1.coeff.detach().clone()
+                best_bh2 = model2.bh.detach().clone()
+                best_coeff2 = model2.coeff.detach().clone()
+                """
+                min_val_loss = val_loss.detach().clone()
+            else:
+                stop_itr += 1
+                if stop_itr == 2000:
+                    break
+                
+    model1.mu = best_mu1
+    model2.mu = best_mu2
+    model1.sigma = best_sig1
+    model2.sigma = best_sig2
+    model1.coeff = best_c1
+    model2.coeff = best_c2
+    """
+    model1.bh = best_bh1
+    model1.coeff = best_coeff1
+    model2.bh = best_bh2
+    model2.coeff = best_coeff2
+    """
+    
+    return model1, model2, copula
+
+def train_mensa_model_3_events(train_dict, valid_dict, model1, model2, model3, copula, n_epochs=1000, lr=5e-3):
+    # Run training loop
+    model1.enable_grad()
+    model2.enable_grad()
+    model3.enable_grad()
+    copula.enable_grad()
+    optimizer = torch.optim.Adam([{"params": model1.parameters(), "lr": lr},
+                                  {"params": model2.parameters(), "lr": lr},
+                                  {"params": model3.parameters(), "lr": lr},
+                                  {"params": copula.parameters(), "lr": lr}])
+    for i in range(n_epochs):
+        optimizer.zero_grad()
+        loss = triple_loss(model1, model2, model3, train_dict, copula)
+        loss.backward()
+        
+        for p in copula.parameters():
+            p.grad = p.grad * 100
+            p.grad.clamp_(torch.tensor([-0.5]), torch.tensor([0.5]))
+            
+        #copula.theta.grad = copula.theta.grad*1000
+        #play with the clip range to see if it makes any differences 
+        #copula.theta.grad.clamp_(torch.tensor([-0.5]), torch.tensor([0.5]))
+        
+        optimizer.step()
+        
+        if i % 500 == 0:
+                print(loss, copula.parameters())
+    
+    return model1, model2, model3, copula
+
 class SingleMENSA:
     """
     Implements MENSA model for single event scenario.
