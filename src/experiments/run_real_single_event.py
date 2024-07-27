9e695794ed184c7f47e6e024ff81a343959dcbe8
thecml <chr1000@gmail.com>
2024-07-27 23:35:58 +0200

    Added single event datasets

--- src/experiments/run_real_single_event.py
+++ src/experiments/run_real_single_event.py
@@ -1,9 +1,8 @@
 """
-run_synthetic_se_event.py
+run_real_single_event.py
 ====================================
-Experiment 1.1
-
-Models: ["deepsurv", "deephit", "mtlr", "dsm", "dcsurvival", "mensa", "dgp"]
+Datasets: seer_se, support_se, mimic_se
+Models: ["deepsurv", "deephit", "mtlr", "dsm", "dcsurvival", "mensa"]
 """
 
 # 3rd party
@@ -30,6 +29,7 @@ from mensa.model import MENSA
 from utility.data import format_data_deephit_single
 from copula import Clayton2D
 from Copula2 import Convex_Nested
+from data_loader import get_data_loader
 
 # SOTA
 from dcsurvival.dirac_phi import DiracPhi
@@ -55,45 +55,47 @@ torch.set_default_dtype(dtype)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Define models
-MODELS = ["deepsurv", "deephit", "mtlr", "dsm", "dcsurvival", "mensa", "dgp"]
+MODELS = ["deepsurv", "deephit", "mtlr", "dsm", "dcsurvival", "mensa"] 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--seed', type=int, default=1)
-    parser.add_argument('--k_tau', type=float, default=0.25)
-    parser.add_argument('--copula_name', type=str, default="clayton")
-    parser.add_argument('--linear', type=bool, default=False)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--dataset_name', type=str, default='seer_se')
     
     args = parser.parse_args()
     seed = args.seed
-    k_tau = args.k_tau
-    copula_name = args.copula_name
-    linear = args.linear
+    dataset_name = args.dataset_name
     
     # Load and split data
-    data_config = load_config(cfg.DGP_CONFIGS_DIR, f"synthetic_se.yaml")
-    dl = SingleEventSyntheticDataLoader().load_data(data_config=data_config,
-                                                    linear=linear, copula_name=copula_name,
-                                                    k_tau=k_tau, device=device, dtype=dtype)
+    dl = get_data_loader(dataset_name)
+    dl = dl.load_data(n_samples=1000)
     train_dict, valid_dict, test_dict = dl.split_data(train_size=0.7, valid_size=0.1, test_size=0.2,
                                                       random_state=seed)
-    n_samples = train_dict['X'].shape[0]
-    n_features = train_dict['X'].shape[1]
-    n_events = data_config['n_events']
-    dgps = dl.dgps
     
+    # Preprocess data
+    cat_features = dl.cat_features
+    num_features = dl.num_features
+    X_train = pd.DataFrame(train_dict['X'], columns=dl.columns)
+    X_valid = pd.DataFrame(valid_dict['X'], columns=dl.columns)
+    X_test = pd.DataFrame(test_dict['X'], columns=dl.columns)
+    X_train, X_valid, X_test= preprocess_data(X_train, X_valid, X_test, cat_features,
+                                              num_features, as_array=True)
+    train_dict['X'] = torch.tensor(X_train, device=device, dtype=dtype)
+    valid_dict['X'] = torch.tensor(X_valid, device=device, dtype=dtype)
+    test_dict['X'] = torch.tensor(X_test, device=device, dtype=dtype)
+    n_samples = train_dict['X'].shape[0]
+
     # Make time bins
-    min_time = dl.get_data()[1].min()
-    max_time = dl.get_data()[1].max()
     time_bins = make_time_bins(train_dict['T'], event=None, dtype=dtype)
 
     # Format data to work easier with sksurv API
+    n_features = train_dict['X'].shape[1]
     X_train = pd.DataFrame(train_dict['X'], columns=[f'X{i}' for i in range(n_features)])
     X_valid = pd.DataFrame(valid_dict['X'], columns=[f'X{i}' for i in range(n_features)])
     X_test = pd.DataFrame(test_dict['X'], columns=[f'X{i}' for i in range(n_features)])
     y_train = convert_to_structured(train_dict['T'], train_dict['E'])
-    y_valid = convert_to_structured(valid_dict['T'], valid_dict['E']) 
+    y_valid = convert_to_structured(valid_dict['T'], valid_dict['E'])
     y_test = convert_to_structured(test_dict['T'], test_dict['E'])
     
     # Evaluate each model
@@ -101,14 +103,6 @@ if __name__ == "__main__":
         if model_name == "cox":
             config = dotdict(cfg.COX_PARAMS)
             model = make_cox_model(config)
-            model.fit(X_train, y_train)
-        elif model_name == "coxnet":
-            config = dotdict(cfg.COXNET_PARAMS)
-            model = make_coxnet_model(config)
-            model.fit(X_train, y_train)
-        elif model_name == "coxboost":
-            config = dotdict(cfg.COXBOOST_PARAMS)
-            model = make_coxboost_model(config)
             model.fit(X_train, y_train)
         elif model_name == "rsf":
             config = dotdict(cfg.RSF_PARAMS)
@@ -156,14 +150,14 @@ if __name__ == "__main__":
             widths = config['widths']
             lc_w_range = config['lc_w_range']
             shift_w_range = config['shift_w_range']
-            learning_rate = 1e-4 # use 1e-4 for synthetic
+            learning_rate = 1e-5 # use 1e-5 for real
             phi = DiracPhi(depth, widths, lc_w_range, shift_w_range, device, tol=1e-14).to(device)
             model = DCSurvival(phi, device, num_features=n_features, tol=1e-14).to(device)
             model = train_dcsurvival_model(model, train_dict['X'], valid_dict['X'],
-                                            train_dict['T'], train_dict['E'],
-                                            valid_dict['T'], valid_dict['E'],
-                                            num_epochs=10, batch_size=32,
-                                            learning_rate=learning_rate, device=device)
+                                           train_dict['T'], train_dict['E'],
+                                           valid_dict['T'], valid_dict['E'],
+                                           num_epochs=10, batch_size=32,
+                                           learning_rate=learning_rate, device=device)
         elif model_name == "mensa":
             config = load_config(cfg.MENSA_CONFIGS_DIR, f"synthetic.yaml")
             n_epochs = config['n_epochs']
@@ -172,14 +166,12 @@ if __name__ == "__main__":
             copula = Clayton2D(torch.tensor([2.0]).type(dtype), device, dtype)
             model = MENSA(n_features=n_features, n_events=2, copula=copula, device=device)
             model.fit(train_dict, valid_dict, n_epochs=100, lr=0.005, batch_size=128) #4096
-        elif model_name == "dgp":
-            pass
         else:
             raise NotImplementedError()
         
         # Compute survival function
         n_samples = test_dict['X'].shape[0]
-        if model_name in ['cox', 'coxnet', "coxboost", 'rsf']:
+        if model_name in ['cox']:
             model_preds = model.predict_survival_function(X_test)
             model_preds = np.row_stack([fn(time_bins) for fn in model_preds])
         elif model_name == 'dsm':
@@ -202,47 +194,37 @@ if __name__ == "__main__":
         elif model_name == "dcsurvival":
             model_preds = predict_survival_function(model, test_dict['X'], time_bins).numpy()
         elif model_name == "mensa":
-            model_preds = model.predict(test_dict['X'], time_bins)[1] # use event preds
-        elif model_name == "dgp":
-            model_preds = torch.zeros((n_samples, time_bins.shape[0]), device=device)
-            for i in range(time_bins.shape[0]):
-                model_preds[:,i] = dgps[0].survival(time_bins[i], test_dict['X'])
+            model_preds = model.predict(test_dict['X'], time_bins)[1].detach().numpy() # use event preds
         else:
             raise NotImplementedError()
-            
-        # Compute L1 (Truth vs. Model) - event only
-        truth_preds_e = torch.zeros((n_samples, time_bins.shape[0]), device=device)
-        for i in range(time_bins.shape[0]):
-            truth_preds_e[:,i] = dgps[0].survival(time_bins[i], test_dict['X'])
-        l1_e = float(compute_l1_difference(truth_preds_e, model_preds,
-                                           n_samples, steps=time_bins))
         
-        # Compute prediction metrics
-        surv_preds = pd.DataFrame(model_preds, columns=time_bins)
-        n_train_samples = len(train_dict['X'])
-        n_test_samples= len(test_dict['X'])
+        # Make evaluation
+        model_results = pd.DataFrame()
+        surv_preds = pd.DataFrame(model_preds, columns=time_bins.numpy())
+        
         y_train_time = train_dict['T']
-        y_train_event = np.array([1] * n_train_samples)
-        y_test_time = test_dict[f'T']
-        y_test_event = np.array([1] * n_test_samples)
+        y_train_event = (train_dict['E'])*1.0
+        y_test_time = test_dict['T']
+        y_test_event = (test_dict['E'])*1.0
         lifelines_eval = LifelinesEvaluator(surv_preds.T, y_test_time, y_test_event,
                                             y_train_time, y_train_event)
-        ci =  lifelines_eval.concordance()[0]
-        ibs = lifelines_eval.integrated_brier_score(num_points=len(time_bins))
-        mae = lifelines_eval.mae(method='Uncensored')
         
-        metrics = [ci, ibs, mae, l1_e]
+        ci = lifelines_eval.concordance()[0]
+        ibs = lifelines_eval.integrated_brier_score()
+        mae_margin = lifelines_eval.mae(method="Margin")
+        
+        metrics = [ci, ibs, mae_margin]
         print(metrics)
-        result_row = pd.Series([model_name, seed, linear, copula_name, k_tau] + metrics,
-                               index=["ModelName", "Seed", "Linear", "Copula", "KTau",
-                                      "CI", "IBS", "MAE", "L1"])
-        
+        res_sr = pd.Series([model_name, dataset_name, seed] + metrics,
+                            index=["ModelName", "DatasetName", "Seed", "CI", "IBS", "MAEM"])
+        model_results = pd.concat([model_results, res_sr.to_frame().T], ignore_index=True)
+            
         # Save results
-        filename = f"{cfg.RESULTS_DIR}/synthetic_se.csv"
+        filename = f"{cfg.RESULTS_DIR}/real_se.csv"
         if os.path.exists(filename):
             results = pd.read_csv(filename)
         else:
-            results = pd.DataFrame(columns=result_row.keys())
-        results = results.append(result_row, ignore_index=True)
+            results = pd.DataFrame(columns=model_results.columns)
+        results = results.append(model_results, ignore_index=True)
         results.to_csv(filename, index=False)
         
