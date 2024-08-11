69580717d93dae79b9622dc06729c9baabf8ede6
Christian Marius Lillelund <chr1000@gmail.com>
2024-08-11 14:45:20 +0200

    Updated models in dgp se script

--- src/experiments/run_dgp_single_event.py
+++ src/experiments/run_dgp_single_event.py
@@ -52,7 +52,7 @@ torch.set_default_dtype(dtype)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Define models
-MODELS = ["cox", "rsf", "deepsurv", "deephit", "dcsurvival", "dsm", "mtlr", "mensa-nocop"]
+MODELS = ["cox", "rsf", "deepsurv", "deephit", "dcsurvival", "dsm", "mtlr", "mensa", "mensa-nocop"]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
